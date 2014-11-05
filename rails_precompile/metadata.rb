21465b776e45e9883f54ca55dd20bbdf8019f961
Alexander <alexbtlv@gmail.com>
2014-11-06 01:16:55 +0500

    Update metadata.rb

--- rails_precompile/metadata.rb
+++ rails_precompile/metadata.rb
@@ -2,7 +2,7 @@ name             'rails_precompile'
 maintainer       'Stefan Wrobel'
 maintainer_email ''
 license          'All rights reserved'
-description      'Installs/Configures opsworks-rails-precompile'
+description      'Installs/Configures rails_precompile'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
